eefec28c3d8a55b9aaeadfd6d710825aed2e3f03
Mridula Vats <mridula@mridula.vats>
2023-11-24 23:28:28 +0530

    second

--- user-service/src/main/java/com/micro/userservice/Controller/UserController.java
+++ user-service/src/main/java/com/micro/userservice/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.micro.userservice.Client.RatingClient;
 import com.micro.userservice.Domain.Rating;
 import com.micro.userservice.Domain.Users;
 import com.micro.userservice.Service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,10 +12,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
@@ -40,6 +46,7 @@ public class UserController {
     }
 
     @GetMapping("withrating/{id}")
+    @CircuitBreaker(name = "ratingBreaker", fallbackMethod = "ratingFallback")
     public ResponseEntity<Users> getUserByIdWithRating(@PathVariable String id){
         Users users = userService.getUser(id);
         users.setRatings(ratingClient.getRatingsByUserId(id));
@@ -51,6 +58,16 @@ public class UserController {
         List<Users> userList = userService.getAllUsers();
         userList.forEach(users -> users.setRatings(ratingClient.getRatingsByUserId(users.getUserId())));
         return ResponseEntity.status(HttpStatus.FOUND).body(userList);
+    }
+
+    public ResponseEntity<Users>  ratingFallback(String id, Exception ex){
+        logger.info("fallback is initiated because service is down: " + ex.getMessage());
+        Users  user = Users.builder().email("dummy@gmail.com")
+                .about("this is dummy user because some service is down")
+                .name("dummy")
+                .userId("34324")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
